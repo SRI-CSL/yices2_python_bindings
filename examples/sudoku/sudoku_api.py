acc96976fff2d4c8b205db90de507965f81a5650
Ian A. Mason <iam@csl.sri.com>
2018-09-18 17:42:57 -0700

    sudoku done both ways. via the yices_api package and via the pythonesque yices package.

--- examples/sudoku/sudoku_api.py
+++ examples/sudoku/sudoku_api.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python
 
+from ctypes import (c_int32)
 
-from yices.Types import Types
+from yices_api import (
+    yices_init,
+    yices_int_type,
+    yices_new_uninterpreted_term,
+    yices_int32,
+    yices_new_config,
+    yices_default_config_for_logic,
+    yices_new_context,
+    yices_and2,
+    yices_arith_leq_atom,
+    yices_assert_formula,
+    make_term_array,
+    yices_distinct,
+    yices_arith_eq_atom,
+    yices_get_model,
+    yices_get_int32_value,
+    yices_free_model,
+    yices_check_context,
+    yices_free_context,
+    yices_free_config,
+    yices_exit,
+    STATUS_SAT
+    )
 
-from yices.Terms import Terms
-
-from yices.Config import Config
-
-from yices.Context import Context
-
-from yices.Status import Status
-
-from yices.Model import Model
-
-from yices.Yices import Yices
+yices_init()
 
 
-
-int_t = Types.int_type()
+int_t = yices_int_type()
 
 
 #seems logical to make the terms in a grid.
@@ -25,7 +37,7 @@ X = [None] * 9
 for i in range(9):
     X[i] = [None] * 9
     for j in range(9):
-        X[i][j] = Terms.new_uninterpreted_term(int_t)
+        X[i][j] = yices_new_uninterpreted_term(int_t)
 
 #not real happy about the indexing going from 0 to 8, but
 #isolating access via V could make it easier to go from 1 to 9
@@ -37,41 +49,45 @@ def V(i, j):
 #make the constants that we will need
 C = {}
 for i in range(1, 10):
-    C[i] = Terms.integer(i)
+    C[i] = yices_int32(i)
 
 one = C[1]
 nine = C[9]
 
 
-config = Config()
-config.default_config_for_logic("QF_LIA")
-
-context = Context(config)
+config = yices_new_config()
+yices_default_config_for_logic(config, "QF_LIA")
+context = yices_new_context(config)
 
 
 
 # x is between 1 and 9
 def between_1_and_9(x):
-    return Terms.conjunction([Terms.arith_leq_atom(one, x), Terms.arith_leq_atom(x, nine)])
+    return yices_and2(yices_arith_leq_atom(one, x), yices_arith_leq_atom(x, nine))
 
 for i in range(9):
     for j in range(9):
-        context.assert_formula(between_1_and_9(V(i, j)))
+        yices_assert_formula(context, between_1_and_9(V(i, j)))
 
+
+def all_distinct(x):
+    n = len(x)
+    a = make_term_array(x)
+    return yices_distinct(n, a)
 
 # All elements in a row must be distinct
 for i in range(9):
-    context.assert_formula(Terms.distinct([V(i, j) for j in range(9)]))
+    yices_assert_formula(context, all_distinct([V(i, j) for j in range(9)]))  #I.e.  all_distinct(X[i])
 
 
 # All elements in a column must be distinct
 for i in range(9):
-    context.assert_formula(Terms.distinct([V(j, i) for j in range(9)]))
+    yices_assert_formula(context, all_distinct([V(j, i) for j in range(9)]))
 
 # All elements in each 3x3 square must be distinct
 for k in range(3):
     for l in range(3):
-        context.assert_formula(Terms.distinct([V(i + 3 * l, j + 3 * k) for i in range(3) for j in range(3)]))
+        yices_assert_formula(context, all_distinct([V(i + 3 * l, j + 3 * k) for i in range(3) for j in range(3)]))
 
 
 #initial conditions (part of the UI)
@@ -80,7 +96,7 @@ def set_value(context, position, value):
     assert 1 <= row and row <= 9
     assert 1 <= column and column <= 9
     assert 1 <= value and value <= 9
-    context.assert_formula(Terms.arith_eq_atom(V(row - 1, column - 1), C[value]))
+    yices_assert_formula(context, yices_arith_eq_atom(V(row - 1, column - 1), C[value]))
 
 
 #
@@ -138,23 +154,24 @@ set_value(context, (9, 7), 2)
 
 #check sat
 
-smt_stat = context.check_context(None)
+smt_stat = yices_check_context(context, None)
 
-if smt_stat != Status.SAT:
+if smt_stat != STATUS_SAT:
     print 'No solution: smt_stat = {0}\n'.format(smt_stat)
 else:
     #print model
-    model = Model.from_context(context, 1)
+    model = yices_get_model(context, 1)
+    val = c_int32()
     for i in range(9):
         for j in range(9):
-            val = model.get_value(V(i, j))
-            print 'V({0}, {1}) = {2}'.format(i, j, val)
-    model.dispose()
+            yices_get_int32_value(model, V(i, j), val)
+            print 'V({0}, {1}) = {2}'.format(i, j, val.value)
+    yices_free_model(model)
 
 print 'Cleaning up\n'
 
-context.dispose()
-config.dispose()
+yices_free_context(context)
+yices_free_config(config)
 
 
-Yices.exit()
+yices_exit()
