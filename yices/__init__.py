7a977c445615ac1223881b8506fd7d4bf3362c51
Ian A. Mason <iam@csl.sri.com>
2018-10-03 09:06:30 -0700

    __init__.py voodoo.

--- yices/__init__.py
+++ yices/__init__.py
@@ -1,3 +1,28 @@
 import yices_api as yapi
 
 yapi.yices_init()
+
+from yices.Config import Config
+from yices.Context import Context
+from yices.Constructors import Constructor
+from yices.Model import Model
+from yices.Parameters import Parameters
+from yices.Status import Status
+from yices.Types import Types
+from yices.Terms import Terms
+from yices.YicesException import YicesException
+from yices.Yices import Yices
+from yices.Yvals import Yval
+
+
+__all__ = ['Config',
+           'Context',
+           'Constructor',
+           'Model',
+           'Parameters',
+           'Status',
+           'Types',
+           'Terms',
+           'YicesException',
+           'Yices',
+           'Yval']
